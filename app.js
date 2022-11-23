b5e853d96e5e1b2151f128e52bc645040d462e09
Saidul Islam Santo <sksanto.pro@gmail.com>
2022-11-23 14:14:27 +0600

    add more featurs

--- app.js
+++ app.js
@@ -1,28 +1,65 @@
 //>==> Selectors:
+
+//==> Divs selector:
 const colorDivs = document.querySelectorAll('.color');
+const currentHexs = document.querySelectorAll('.color h2');
+const copyPopup = document.querySelector('.copy-container');
+const slidersContainers = document.querySelectorAll('.adjustment-sliders');
+
+// Button selector:
 const generateBtn = document.querySelector('.generate-Btn');
 const sliders = document.querySelectorAll('input[type="range"]');
-const currentHexs = document.querySelectorAll('.color h2');
+const adjustmentBtns = document.querySelectorAll('.adjust-Btn');
+const lockBtns = document.querySelectorAll('.lock-Btn');
+const adjustmentCloseBtns = document.querySelectorAll('.close-adjustment');
 
 //>==> Variables:
 let initialColors;
 
 //>==> EventListners:
 
-//=> Sliders Input EventListener:
+//=> 1. Sliders Input EventListener:
 sliders.forEach((slider) => {
 	slider.addEventListener('input', hslControls);
 });
 
-// Update Hex Text on change:
+// 2. Update Hex Text on change:
 colorDivs.forEach((colorDiv, index) => {
 	colorDiv.addEventListener('change', () => {
 		updateTextUI(index);
 	});
 });
 
-//=> Generae Btn addEventListener:
-generateBtn.addEventListener('click', generateNewColorPalette);
+// 3. Hex value Copy To Clipboard EventListener:
+currentHexs.forEach((currentHex) => {
+	currentHex.addEventListener('click', () => {
+		copyToClipboard(currentHex);
+	});
+});
+
+// 4. Copy popup EventListener:
+copyPopup.addEventListener('transitionend', () => {
+	const copyPopupBox = copyPopup.children[0];
+	copyPopup.classList.remove('active');
+	copyPopupBox.classList.remove('active');
+});
+
+// 5. Toggle Controls Panel by adjustBtn:
+adjustmentBtns.forEach((Btn, index) => {
+	Btn.addEventListener('click', () => {
+		openAdjustmentPanel(index);
+	});
+});
+
+// 6. Close Controls Panel by panel close Btn:
+adjustmentCloseBtns.forEach((Btn, index) => {
+	Btn.addEventListener('click', () => {
+		closeAdjustmentPanel(index);
+	});
+});
+
+//=>  Generae random Color Btn:
+generateBtn.addEventListener('click', randomColors);
 
 //>==> Function:
 //==>> Random Color and Hex Gererator Function:
@@ -52,7 +89,13 @@ function randomColors() {
 		const randomColor = generateHex();
 
 		// Adding it to Array:
-		initialColors.push(chroma(randomColor).hex());
+
+		if (colorDiv.classList.contains('locked')) {
+			initialColors.push(hexText.innerText);
+			return;
+		} else {
+			initialColors.push(chroma(randomColor).hex());
+		}
 
 		// Adding Color to BG and Hex text:
 		colorDiv.style.backgroundColor = randomColor;
@@ -76,6 +119,12 @@ function randomColors() {
 
 	//==> Reset Input value:
 	resetInputs();
+
+	//==> Check Adjust and Lock Btns Contrast:
+	adjustmentBtns.forEach((Btn, index) => {
+		textContrastCheck(initialColors[index], Btn);
+		textContrastCheck(initialColors[index], lockBtns[index]);
+	});
 }
 
 //==> Contrast Checker:
@@ -206,9 +255,46 @@ function resetInputs() {
 	});
 }
 
-//===> New Color Palette Generator:
-function generateNewColorPalette() {
-	randomColors();
+// Hex color value Copier:
+function copyToClipboard(currentHex) {
+	// step-1: Create a Temporary element(textarea):
+	const element = document.createElement('textarea');
+
+	// step-2: Add a value to temp element:
+	element.value = currentHex.innerText;
+
+	// step-3: Now Temp element append to the body:
+	document.body.appendChild(element);
+
+	//step-4: Add a Select() method to it:
+	element.select();
+
+	//step-5: Add a spacial method called 'execCommand()' for copy value:
+	document.execCommand('copy');
+
+	//step-6: Finaly Remove the element from the body:
+	document.body.removeChild(element);
+
+	//==> Copy popup Show:
+	const copyPopupBox = copyPopup.children[0];
+	copyPopup.classList.add('active');
+	copyPopupBox.classList.add('active');
 }
+
+//==> Adjustment panel Opener and Closer:
+// Panel Opener(toggler):
+function openAdjustmentPanel(index) {
+	slidersContainers[index].classList.toggle('active');
+}
+
+//Panel Closer:
+function closeAdjustmentPanel(index) {
+	slidersContainers[index].classList.remove('active');
+}
+
+// //==> Lock color palette:
+// function lockPalette(e, index) {
+
+// }
 
 randomColors();
