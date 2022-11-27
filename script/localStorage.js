1f24828d52678ed639ff68bffae45bda55b0e552
Saidul Islam Santo <sksanto.pro@gmail.com>
2022-11-27 16:37:57 +0600

    add to localStorage

--- script/localStorage.js
+++ script/localStorage.js
@@ -20,12 +20,15 @@ saveBtn.addEventListener('click', openSavePalette);
 //=> Close Save Palette:
 saveCloseBtn.addEventListener('click', closeSavePalette);
 
+//=> Submit Save Palette:
+saveSubmitBtn.addEventListener('click', submitSavePalette);
+
 //-------->> EventListener end <==||
 
 //==> Funnction start:
 
 //Open palette Saver:
-function openSavePalette() {
+function openSavePalette(event) {
 	const savePopup = saveContainer.children[0];
 
 	saveContainer.classList.add('active');
@@ -33,10 +36,52 @@ function openSavePalette() {
 }
 
 //Close palette saver:
-function closeSavePalette() {
+function closeSavePalette(event) {
 	const savePopup = saveContainer.children[0];
 
 	saveContainer.classList.remove('active');
 	savePopup.classList.remove('active');
 }
+
+//Submit palette saver:
+function submitSavePalette(event) {
+	//Remove Popup:
+	const savePopup = saveContainer.children[0];
+
+	saveContainer.classList.remove('active');
+	savePopup.classList.remove('active');
+
+	//Create variables and set values:
+	const name = saveInput.value;
+	const colors = [];
+
+	currentHexs.forEach((hex) => {
+		colors.push(hex.innerText);
+	});
+
+	// Generate Palettes Object:
+	let paletteNr = savedPalettes.length;
+
+	const paletteObj = { name, colors, Nr: paletteNr };
+	savedPalettes.push(paletteObj);
+
+	// Save to localStorage:
+	saveToLocalStorage(paletteObj);
+	saveInput.value = '';
+}
+
+//Local Storage data Saver:
+function saveToLocalStorage(paletteObj) {
+	let localPalettes;
+
+	if (localStorage.getItem('palettes') === null) {
+		localPalettes = [];
+	} else {
+		localPalettes = JSON.parse(localStorage.getItem('palettes'));
+	}
+
+	localPalettes.push(paletteObj);
+	localStorage.setItem('palettes', JSON.stringify(localPalettes));
+}
+
 //-------->> Function end <===||
